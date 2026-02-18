b1f4e20c966b6ac0bc0e238b0d41ad45fdce87d0
Emirhan <emirhan.bda0@gmail.com>
2026-02-18 23:34:11 +0300

    .

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,3 +1,23 @@
-const authControl = (req, res, next){
-    
-}
+module.exports = (req,res,next) => {
+    const authHeader = req.header("Authorization");
+    if(!authHeader){
+     res.status(401).json({message: "Yetkisiz erisim: Token bulunamadi"});
+    return next();
+    };
+
+    const token = authHeader.split(' ')[1];
+    if(!token){
+        res.status(401).json({message: "Yetkisiz erisim: Gecersiz token"});
+        return next();
+    };
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch(err){
+        console.log("TERMİNAL HATASI:", err.message); 
+        res.status(401).json({message: "Yetkisiz erisim: Gecersiz token"});
+        return next();
+    }
+};
