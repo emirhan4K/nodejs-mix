b3516d07f12d05666d6b1c64b4974ed9e7031a9e
Emirhan <emirhan.bda0@gmail.com>
2026-02-27 16:44:04 +0300

    repo

---
+++ repository/user.repository.js
@@ -0,0 +1,22 @@
+const User = require("../models/user.model");
+
+class UserRepository {
+  async findByEmail(email) {
+    return await User.findOne({ email });
+  }
+
+  async findByUsername(username) {
+    return await User.findOne({ username });
+  }
+
+  async create(userData) {
+    const user = new User(userData);
+    return await user.save();
+  }
+
+  async updateById(id, updateData) {
+    return await User.findByIdAndUpdate(id, updateData, { new: true });
+  }
+}
+
+module.exports = new UserRepository();
