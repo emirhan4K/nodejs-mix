497f40f5424de103244a667d02c52263fe06cbde
Emirhan <emirhan.bda0@gmail.com>
2026-02-27 16:31:11 +0300

    d

---
+++ models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength: 6
+    }
+})
