c4020ebcc2c98de1fcd9812b0eb3aa6ae7d11f9a
Emirhan <emirhan.bda0@gmail.com>
2026-02-27 20:41:14 +0300

    S

---
+++ controllers/user.controller.js
@@ -0,0 +1,31 @@
+const userService = require("../services/user.services");
+
+class UserController {
+  async register(req, res) {
+    try {
+      const user = await authService.registerUser(req.body);
+      res.status(201).json({ message: "Başarıyla kayıt olundu.", user });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await authService.loginUser(email, password);
+      res.status(200).json({ message: "Giriş başarılı.", userId: user._id });
+    } catch (error) {
+      res.status(401).json({ error: error.message });
+    }
+  }
+
+  async forgotPassword(req, res) {
+    try {
+      const token = await authService.createPasswordResetToken(req.body.email);
+      res.status(200).json({ message: "Sıfırlama linki gönderildi.", token });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  }
+}
