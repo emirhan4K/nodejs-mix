4e5b5ca734ad0ae5d535c4d1569870eabf9efb87
Emirhan <emirhan.bda0@gmail.com>
2026-04-09 15:41:23 +0300

    d

---
+++ models/Order.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  items: [
+    {
+      urunId: {
+        type: String,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Hazırlanıyor..",
+  },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
