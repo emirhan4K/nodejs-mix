bf5b1e2a04ebe1fe33d45cedbf37a866a7df0417
Emirhan <emirhan.bda0@gmail.com>
2026-04-09 16:38:45 +0300

    d

--- services/order.service.js
+++ services/order.service.js
@@ -1,1 +1,12 @@
-const 
+const orderRepository = require("../repository/order.repository");
+const cartRepository = require("../repository/cart.repository");
+
+class OrderService{
+    async siparisVer(userId, address){
+        const cart = await cartRepository.kullaniciSepetiBul(userId);
+        if(!cart || cart.items.length === 0){
+            throw new Error("Sepetiniz boş, sipariş verilemez!");
+        }
+        
+    }
+}
