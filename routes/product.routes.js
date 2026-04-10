713c8838e8a7253971c79ce8f376f0af0bf591c9
Emirhan <emirhan.bda0@gmail.com>
2026-04-10 10:19:51 +0300

    d

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
 const upload = require("../middlewares/upload.middleware");
+const validate = require("../middlewares/validation.middleware");
+const productSchema = require("../validations/product.validation");
 
-router.post("/",upload.single("image"),productController.productAdd);
+router.post("/",upload.single("image"),validate(productSchema),productController.productAdd);
 router.get("/getir",productController.productGetir);
 router.get("/:slug", productController.slugaGoreGetir);
 
